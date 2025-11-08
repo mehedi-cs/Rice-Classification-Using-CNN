b324842743445fe9574f9cadb847c251410b6a8f
Md. Mehedi Hasan <mehedicsgo@gmail.com>
2025-11-09 00:31:26 +0600

    Add function to split dataset into train/val/test

--- src/dataset.py
+++ src/dataset.py
@@ -5,3 +5,9 @@ import torchvision
 import torchvision.transforms as transforms
 import torch
 from termcolor import colored
+
+
+def split_dataset(base_dir, output_dir='Rice_images'):
+    splitfolders.ratio(input=base_dir, output=output_dir, seed=42, ratio=(0.7, 0.15, 0.15))
+    print(colored("Dataset split into train/val/test", 'green', attrs=['bold']))
+    return output_dir
