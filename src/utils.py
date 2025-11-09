3592302263dacf0cde6f122b6010d07bb06d1d22
Md. Mehedi Hasan <mehedicsgo@gmail.com>
2025-11-09 18:37:38 +0600

    Add setup_environment function for hardware info
    Added a function to set up the environment and print hardware information.

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,17 @@ import torch
 from termcolor import colored
 import seaborn as sns
 import warnings
+
+def setup_environment():
+    """Setup environment and print hardware info."""
+    warnings.filterwarnings('ignore')
+    sns.set_style('darkgrid')
+
+    print(colored('Your hardware uses : ', 'blue', attrs=['bold']))
+    if torch.cuda.is_available():
+        print(colored('GPU', 'green', attrs=['bold']))
+        num_gpus = torch.cuda.device_count()
+        for i in range(num_gpus):
+            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+    else:
+        print(colored('CPU', 'green', attrs=['bold']))
